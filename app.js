bdbd1dd9db8ef1157219e175a341c89bc7b3bbb4
Hannah Hill <hannahhill@Hannahs-MacBook-Pro.local>
2024-07-15 15:17:12 +0100

    Get api endpoint passing tests.

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const app = express();
 
 app.use(express.json());
 
-const {} = require("./controller/controller");
+const { getTopics, getApi } = require("./controller/controller");
 
-app.get("/api/topics", getTopics);
 // This is where Express has access to the requests and responses objects and functions as middleware
+app.get("/api/topics", getTopics);
+
+app.get("/api", getApi);
 
 // At the bottom of the app file...
 app.use((err, req, res, next) => {
--- controller/controller.js
+++ controller/controller.js
@@ -1,9 +1,10 @@
 const { fetchTopics } = require("../model/model");
+const endpoints = require("../endpoints.json");
 // functions are required in from the model
 
 // Request and response handling and error handling might look like...
 
-getTopics = (req, res, next) => {
+const getTopics = (req, res, next) => {
   fetchTopics()
     .then((topics) => {
       res.status(200).send({ topics });
@@ -13,4 +14,8 @@ getTopics = (req, res, next) => {
     });
 };
 
-module.exports = { getTopics };
+const getApi = (req, res, next) => {
+  res.status(200).send({ endpoints: endpoints });
+};
+
+module.exports = { getTopics, getApi };
