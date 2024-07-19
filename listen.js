2efc617a5a5eabc442280a6ed3623ef0401f8997
Hannah Hill <hannahhill@Hannahs-MacBook-Pro.local>
2024-07-19 13:31:24 +0100

    Hosting changes - switching to main.

--- listen.js
+++ listen.js
@@ -1,12 +1,9 @@
+const app = require("./app.js");
+const [PORT = 9090] = process.env;
+
+app.listen(PORT, () => console.log("Listening on port 9090"));
+
 const express = require("express");
 const app = express();
 
 app.use(express.json());
-
-app.listen(8888, (err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("Listening on port 8899");
-  }
-});
