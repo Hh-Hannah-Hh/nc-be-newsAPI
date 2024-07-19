708ecc729ee87f13236c69cd5e8c376101898358
Hannah Hill <hannahhill@Hannahs-MacBook-Pro.local>
2024-07-19 13:51:41 +0100

    update connection and listen files.

--- listen.js
+++ listen.js
@@ -2,8 +2,3 @@ const app = require("./app.js");
 const [PORT = 9090] = process.env;
 
 app.listen(PORT, () => console.log("Listening on port 9090"));
-
-const express = require("express");
-const app = express();
-
-app.use(express.json());
